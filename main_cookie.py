13707fe21d2064e8f9d81d20b9f7bf71005686bb
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2024-12-03 20:03:54 +0000

    Day 48 progress

--- main_cookie.py
+++ main_cookie.py
@@ -1,14 +1,20 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_experimental_option("detach", True)
+
+# service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
-
+driver.implicitly_wait(1)
 # all_portals.click()
 
 cookie = driver.find_element(By.ID, value="cookie")
@@ -20,7 +26,12 @@ cookie = driver.find_element(By.ID, value="cookie")
 # buyPortal = driver.find_element(By.ID, value="buyPortal")
 # buyTime_machine = driver.find_element(By.ID, value="buyTime machine")
 
-while True:
-  cookie.click()
-
+try:
+    while True:
+        cookie.click()
+        time.sleep(0.1)  # Add a short delay to prevent overwhelming the browser
+except KeyboardInterrupt:
+    print("Script stopped by user.")
+    driver.quit()
+ 
 # driver.quit()
