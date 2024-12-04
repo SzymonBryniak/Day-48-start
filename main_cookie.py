66ed7031b1fe8790f754b6c81350c7b85b6185f0
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2024-12-04 20:03:32 +0000

    Day 48 progress

--- main_cookie.py
+++ main_cookie.py
@@ -5,19 +5,26 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_experimental_option("detach", True)
+chromedriver_path = "C:\webdrivers\chromedriver"  # Replace with your ChromeDriver path
+
+# Create a service object for ChromeDriver
+service = Service(chromedriver_path)
 
 # service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options, service=service)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
-driver.implicitly_wait(1)
+# driver.implicitly_wait(2)
 # all_portals.click()
 
 cookie = driver.find_element(By.ID, value="cookie")
+# cookie = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "cookie")))
 # buyGrandma = driver.find_element(By.ID, value="buyGrandma")
 # buyFactory = driver.find_element(By.ID, value="buyFactory")
 # buyMine = driver.find_element(By.ID, value="buyMine")
@@ -28,10 +35,13 @@ cookie = driver.find_element(By.ID, value="cookie")
 
 try:
     while True:
+        wait = WebDriverWait(driver, timeout=2)
+        wait.until(lambda d : cookie.is_displayed())
+        time.sleep(0.1)
         cookie.click()
-        time.sleep(0.1)  # Add a short delay to prevent overwhelming the browser
+          # Add a short delay to prevent overwhelming the browser
 except KeyboardInterrupt:
     print("Script stopped by user.")
     driver.quit()
  
-# driver.quit()
+driver.quit()
