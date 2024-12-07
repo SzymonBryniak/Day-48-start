683e18cb4a75158aabcbc8f37fb55039f6df0d3e
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2024-12-07 20:11:10 +0000

    Day 48 progress

--- main_cookie.py
+++ main_cookie.py
@@ -1,4 +1,5 @@
 import time
+from win32 import win32file
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,29 +11,25 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from get_chrome_driver import GetChromeDriver
 
-get_driver = GetChromeDriver()
-# get_driver.auto_download(extract=True)
-get_driver.install()
-
-
-
+# get_driver = GetChromeDriver()
+# # get_driver.auto_download(extract=True)
+# get_driver.install()
+win32file._setmaxstdio(2048)
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--no-sandbox")
+chrome_options.page_load_strategy = 'eager'
 chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument("--no-sandbox")
 
 # Create a service object for ChromeDriver
 # chromedriver_path = "C:\webdrivers\chromedriver.exe"  # causes browser mismatch
 # service = Service(chromedriver_path)
 
-service = Service(ChromeDriverManager().install())
+# service = Service(ChromeDriverManager().install())
+service = webdriver.ChromeService(port=1234)
 driver = webdriver.Chrome(options=chrome_options, service=service)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
-# driver.implicitly_wait(2)
-# all_portals.click()
-
-
-
-
+driver.current_window_handle
+driver.implicitly_wait(2)
 
 
 cookie = driver.find_element(By.ID, value="cookie")
@@ -47,9 +44,9 @@ cookie = driver.find_element(By.ID, value="cookie")
 
 try:
     while True:
-        wait = WebDriverWait(driver, timeout=2)
-        wait.until(lambda d : cookie.is_displayed())
-        time.sleep(0.1)
+        # wait = WebDriverWait(driver, timeout=2)
+        # wait.until(lambda d : cookie.is_displayed())
+        # time.sleep(0.01)
         cookie.click()
           # Add a short delay to prevent overwhelming the browser
 except KeyboardInterrupt:
