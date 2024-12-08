d87a799370af1d319f9a05973b1ee673c8094def
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2024-12-08 20:02:04 +0000

    Day 48 progress

--- main_cookie.py
+++ main_cookie.py
@@ -1,4 +1,7 @@
 import time
+import re
+import functools
+import operator
 from win32 import win32file
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,6 +13,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from get_chrome_driver import GetChromeDriver
+import chromedriver_autoinstaller
+chromedriver_autoinstaller.install()
 
 # get_driver = GetChromeDriver()
 # # get_driver.auto_download(extract=True)
@@ -25,30 +30,59 @@ chrome_options.add_argument("--no-sandbox")
 # service = Service(chromedriver_path)
 
 # service = Service(ChromeDriverManager().install())
-service = webdriver.ChromeService(port=1234)
-driver = webdriver.Chrome(options=chrome_options, service=service)
+# service = webdriver.ChromeService(port=1234)
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
+
+
 driver.current_window_handle
 driver.implicitly_wait(2)
 
 
 cookie = driver.find_element(By.ID, value="cookie")
+
+
+
 # cookie = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "cookie")))
-# buyGrandma = driver.find_element(By.ID, value="buyGrandma")
-# buyFactory = driver.find_element(By.ID, value="buyFactory")
+buyGrandma = driver.find_element(By.ID, value="buyGrandma")
+buyFactory = driver.find_element(By.ID, value="buyFactory")
 # buyMine = driver.find_element(By.ID, value="buyMine")
 # buyShipment = driver.find_element(By.ID, value="buyShipment")
 # buyAlchemy = driver.find_element(By.ID, value="buyAlchemy lab")
 # buyPortal = driver.find_element(By.ID, value="buyPortal")
 # buyTime_machine = driver.find_element(By.ID, value="buyTime machine")
 
+
+# to decorate
+def is_available(*args):
+    greyed = "grayed" in buyGrandma.get_attribute("class")
+    print(greyed)
+    print(buyGrandma.text)
+    find_digits = re.findall("\d", buyFactory.text)
+    concatenate_no_spaces = functools.reduce(operator.add, find_digits)
+    print(concatenate_no_spaces)
+    if greyed == False:
+        buyGrandma.click()
+
+# is_available(buyGrandma)
+start_time = round(time.time())
+
 try:
     while True:
+
         # wait = WebDriverWait(driver, timeout=2)
         # wait.until(lambda d : cookie.is_displayed())
         # time.sleep(0.01)
+        current_time = round(time.time())
+        elapsed_time = current_time - start_time
+        if round(elapsed_time) % 5 == 0:
+            print(elapsed_time)
+            is_available(buyGrandma)
+            
+        
         cookie.click()
-          # Add a short delay to prevent overwhelming the browser
+        # Add a short delay to prevent overwhelming the browser
+ 
 except KeyboardInterrupt:
     print("Script stopped by user.")
     driver.quit()
